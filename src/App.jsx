3a62ee7a6101cf150494ce85e7b147dfeda1aa6d
hxnoor07 <harnoor2004saini@gmail.com>
2024-06-10 18:43:11 +0530

    create project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import HeroSection from './Components/Hero';
+import Nav from "./Components/Nav";
+
+const App=()=>{
+  return(
+    <div>
+      <Nav/>
+      <HeroSection/>
+    </div>
+  );
+}
+export default App;
